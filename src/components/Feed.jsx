f35ad4d2534366fde38677d6ee71411a487bf282
Umar-Jimoh <umarjimoh@hotmail.com>
2023-08-10 09:48:57 +0100

    Creating the sidebar

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,15 @@
-
+import {Box, Stack, Typography} from '@mui/material'
+import SideBar from './SideBar'
 
 const Feed = () => (
-    <>Feed</>
+    <Stack sx={{ flexDirection: {sx:'column', md:"row"}}}>
+      <Box  sx={{height: {sx:'auto', md:'89vh'}, borderRight:'1px solid #3d3d3d', px: { sx: 0, md: 2}}}>
+        <SideBar />
+        <Typography className='copyright' variant='body2' sx={{ mt: 1.5, color: '#fff'}}>
+          Copyright 2023 JSM Media
+        </Typography>
+      </Box>
+    </Stack>
   )
 
 export default Feed
